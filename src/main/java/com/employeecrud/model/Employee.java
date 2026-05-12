dbba1aabb4eff293aeca64b682d5d86d62c26108
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2026-05-12 09:15:31 +0430

    Employee file added

---
+++ src/main/java/com/employeecrud/model/Employee.java
@@ -0,0 +1,18 @@
+package com.employeecrud.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "employees")
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+}
